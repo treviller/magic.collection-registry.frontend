62125cfb0926cca2675312de251b980192840916
Tanguy REVILLER <tanguy.reviller@protonmail.com>
2023-01-25 18:20:52 +0100

    feat(card): Use real preview image instead of stub one

--- src/components/model/card.tsx
+++ src/components/model/card.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import * as React from 'react'
 
 export type CardData = {
-    id: String
-    name: String,
-    language: String,
-    rarity: String,
+    id: string
+    name: string,
+    language: string,
+    rarity: string,
+    preview_image: string|null
 }
 
 type Props = {
@@ -13,13 +14,15 @@ type Props = {
 }
 
 const Card: React.FC<Props> = ({data}) => {
+    let image_url = data.preview_image ?? "https://cards.scryfall.io/normal/front/6/d/6da045f8-6278-4c84-9d39-025adf0789c1.jpg?1562404626"
+
     return (
         <div className="flex-1 flex-col max-w-xs m-3 basis-60">
             <div className="w-full">
-                <Image src="https://cards.scryfall.io/normal/front/6/d/6da045f8-6278-4c84-9d39-025adf0789c1.jpg?1562404626"
-                    alt={"Picture of the card "+data.name}
-                    width="488"
-                    height="680"></Image>
+                <Image src={image_url}
+                       alt={"Picture of the card "+data.name}
+                       width="488"
+                       height="680"></Image>
             </div>
             <span>{data.name}</span>
         </div>
