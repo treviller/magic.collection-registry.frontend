86f696111d1203d82c09bc593037a0ac964d1cd7
Tanguy REVILLER <tanguy.reviller@protonmail.com>
2023-01-25 18:25:49 +0100

    style(header): Split header in two separate parts

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,17 +14,21 @@ export default function Header() {
 
     return (
         <header className="static bg-blue-800">
-            <ul className="p-4 flex flex-row justify-center content-center items-center">
-                <li className="p-2 hover:text-white"><Link href="/">Home (icon ?)</Link></li>
-                <li className="p-2 hover:text-white"><Link href="/cards">All cards</Link></li>
-                <li className="p-2 hover:text-white">My collection</li>
-                <li><input className="bg-blue-600 text-white placeholder:text-white rounded-3xl p-2 m-2 text-right focus-visible:border-2 focus-visible:border-red-900"
-                           type="text"
-                           placeholder="Search Something..."
-                           onChange={debouncedEventHandler}
-                /></li>
-                <li className="p-2 hover:text-white">Account</li>
-            </ul>
+            <div className="p-4 flex flex-row justify-between content-center items-center">
+                <ul className="flex flex-row justify-center">
+                    <li className="p-2 hover:text-white"><Link href="/">Home (icon ?)</Link></li>
+                    <li className="p-2 hover:text-white"><Link href="/cards">All cards</Link></li>
+                    <li className="p-2 hover:text-white">My collection</li>
+                </ul>
+                <ul className="flex flex-row justify-center content-center items-center">
+                    <li><input className="bg-blue-600 text-white placeholder:text-white rounded-3xl p-2 m-2 text-right focus-visible:border-2 focus-visible:border-red-900"
+                               type="text"
+                               placeholder="Search Something..."
+                               onChange={debouncedEventHandler}
+                    /></li>
+                    <li className="p-2 hover:text-white">Account</li>
+                </ul>
+            </div>
         </header>
     )
 }
